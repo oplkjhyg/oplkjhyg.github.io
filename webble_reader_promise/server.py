d9576289822a730493d81d2c47773bb5f6649e83
Gordon Chan <gordon.chan@acs.com.hk>
2024-01-04 14:30:01 +0800

    Added async version for testing

---
+++ webble_reader_promise/server.py
@@ -0,0 +1,17 @@
+import http.server, ssl
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+server_address = ('localhost', 8000)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket,
+                               server_side=True,
+                               certfile='localhost.pem',
+                               ssl_version=ssl.PROTOCOL_TLS)
+httpd.serve_forever()
